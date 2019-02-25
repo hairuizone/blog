bd10626c6cf829c4c6cd74e5599a3c1037584152
hairui <lihairuizone@sina.com>
2019-02-25 23:45:10 +0800

    初始化构建blog

--- src/main/java/cn/hairui/blog/controller/DefaultViewController.java
+++ src/main/java/cn/hairui/blog/controller/DefaultViewController.java
@@ -15,7 +15,7 @@ public class DefaultViewController {
     @RequestMapping(value = "/" ,method = RequestMethod.GET)
     public String indexPage(Model model){
         model.addAttribute("greetings","你的努力就是对自己最好的回报");
-        model.addAttribute("bg_path","/static/img/index_bg.jpg");
+        model.addAttribute("bg_path","http://localhost:8848/myblog/img/index_bg.jpg");
         model.addAttribute("name","LaoGanbu");
         return "index";
 
