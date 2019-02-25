6438c9be7ee3838d49b118a6629c1bf410e7a095
thinkpad <lihairuizone@sina.com>
2019-02-25 11:56:46 +0800

    AmazeUI 示例页面访问逻辑添加

--- src/main/java/cn/hairui/blog/AmazeUI/AmazeUITemplatesController.java
+++ src/main/java/cn/hairui/blog/AmazeUI/AmazeUITemplatesController.java
@@ -12,33 +12,89 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AmazeUITemplatesController {
 
     @RequestMapping("/admin-404")
-    public String admin404(){
+    public String admin404() {
         return "admin-404";
     }
 
-    @RequestMapping("/landing")
-    public String landing(){
-        return "landing";
+    @RequestMapping("/admin-form")
+    public String adminForm() {
+        return "admin-form";
     }
-    @RequestMapping("/login")
-    public String login(){
-        return "login";
+
+    @RequestMapping("/admin-gallery")
+    public String adminGallery() {
+        return "admin-gallery";
+    }
+
+    @RequestMapping("/admin-help")
+    public String adminHelp() {
+        return "admin-help";
+    }
+
+    @RequestMapping("/admin-index")
+    public String adminIndex() {
+        return "admin-index";
+    }
+
+    @RequestMapping("/admin-log")
+    public String adminLog() {
+        return "admin-log";
+    }
+
+    @RequestMapping("/admin-table")
+    public String adminTable() {
+        return "admin-table";
+    }
+
+    @RequestMapping("/admin-user")
+    public String adminUser() {
+        return "admin-user";
     }
 
     @RequestMapping("/blog")
     public String blog() {
         return "blog";
     }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/iscroll")
+    public String iscroll() {
+        return "iscroll";
+    }
+
+    @RequestMapping("/landing")
+    public String landing() {
+        return "landing";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/non-responsive")
+    public String nonResponsive() {
+        return "non-responsive";
+    }
+
     @RequestMapping("/sidebar")
-    public String sidebar(){
+    public String sidebar() {
         return "sidebar";
     }
-    @RequestMapping("/widget")
-    public String widget(){
-        return "widget";
-    }
+
     @RequestMapping("widget.basic")
-    public String widgetBasic(){
+    public String widgetBasic() {
         return "widget.basic";
     }
+
+    @RequestMapping("/widget")
+    public String widget() {
+        return "widget";
+    }
+
+
 }
